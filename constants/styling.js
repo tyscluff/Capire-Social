793ac71b42b7df4fe8197772bf99300a69c19581
tyscluff <80591013+tyscluff@users.noreply.github.com>
2023-07-11 13:57:57 -0700

    Add files via upload

---
+++ constants/styling.js
@@ -0,0 +1,7 @@
+const standardFont = 'Trebuc';
+export const titleFont = 'Hussar';
+export const standardBlue = "#3FCBFF";
+export const standardOrange = "#FF6C37";
+// export const standardPink = "#EEAFAF";
+export const standardGray = "#A0A0A0"; 
+// export const standardGray = "#A9A9A9";
